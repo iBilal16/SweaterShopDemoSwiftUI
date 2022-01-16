2aa468a1e9ee54b68ccfd7ca022c6abbb2fcdca0
Bilal <ibilalahmad13@gmail.com>
2022-01-17 00:14:57 +0500

    Initial Commit

---
+++ SweaterShopDemo/SweaterShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopDemoApp.swift
+//  SweaterShopDemo
+//
+//  Created by Bilal Ahmad on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
